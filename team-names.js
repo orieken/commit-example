e8d4ec4db7c6dee65a1dbf741473f1158bae4e06
Rieken <oscar.rieken@adp.com>
2022-10-07 19:34:48 -0400

    :sparkles: feat(Team A): FOO-12345 initial commit

---
+++ team-names.js
@@ -0,0 +1,11 @@
+const TeamNames = [
+  { name: 'Team ui' },
+  { name: 'Team android' },
+  { name: 'Team ios' },
+  { name: 'Team A' },
+  { name: 'Team planner' },
+  { name: 'Team settings' },
+];
+
+
+module.exports = TeamNames;
